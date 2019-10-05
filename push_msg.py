2561f9833766b68e06a903f9273b1361e67253e3
hwakabh <hrykwkbys1024@gmail.com>
2019-10-06 02:27:40 +0900

    Implemented simple push notifications to personal LINE account.

---
+++ push_msg.py
@@ -0,0 +1,18 @@
+import os
+ 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+ 
+# Set credentilas securely
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+line_user_id = os.getenv('LINE_USER_ID', None)
+
+# Instanciate LINEBot object 
+line_bot_api = LineBotApi(channel_access_token)
+ 
+def main():
+    messages = TextSendMessage(text="Hello Python from LINE bot.")
+    line_bot_api.push_message(line_user_id, messages=messages)
+ 
+if __name__ == "__main__":
+    main()
