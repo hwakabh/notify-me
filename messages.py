77189899889923787cd0d825f719be6fae1e6c4b
hwakabh <hrykwkbys1024@gmail.com>
2019-10-14 02:06:29 +0900

    #1 Added closed loops and enable to handle push texts according to reminder time.

---
+++ messages.py
@@ -0,0 +1,25 @@
+## Edit messages whatever you like
+## Both English/Japanese message supported with plain texts
+
+msg = [
+    {
+        'time': '08-00',
+        'text': 'Good Morning.\n Time to go dog-walking.'
+    },
+    {
+        'time': '20-00',
+        'text': 'Good Evening.\n Time to go dog-walking.'
+    },
+    {
+        'time': '08-30',
+        'text': 'It is time to take morning medicine.'
+    },
+    {
+        'time': '13-00',
+        'text': 'It is time to take afternoon medicine.'
+    },
+    {
+        'time': '01-55',
+        'text': 'Do not forget to take evening medicine ??\n Good night and have a nice dream.'
+    }
+]
--- push_notification.py
+++ push_notification.py
@@ -2,7 +2,12 @@ import urllib.request
 import urllib.parse
 import os
 import json
+import datetime
+import time
 
+import messages
+
+# Configurations for pushing to LINE Messaging API
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 LINE_USER_ID = os.getenv('LINE_USER_ID', None)
 
@@ -12,29 +17,59 @@ HEADERS = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {}'.format(LINE_CHANNEL_ACCESS_TOKEN)
 }
+RECONCILATION_PERIOD_SEC = 60
 
-RAW_DATA = {
-    'to': LINE_USER_ID,
-    'messages': [
-        {
-            'type': 'text',
-            'text': 'Hello.'
-        },
-        {
-            'type': 'text',
-            'text': 'Push notification from Python.'
+
+def pushing_messages(text_message):
+    DATA = json.dumps(text_message).encode('utf-8')
+    req = urllib.request.Request(
+        url=URL,
+        headers=HEADERS,
+        data=DATA
+    )
+
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+    if res.getcode() == 200:
+        print('POST success, you might get push notifications soon.')
+    else:
+        print('Return-Code: {} | Failed to POST ...'.format(res.getcode()))
+
+    print('Response body : \n{}'.format(json.loads(body.decode('utf-8'))))
+
+
+def get_message_body(t):
+    msg = ''
+    candidate_times = [m.get('time') for m in messages.msg]
+    print(candidate_times)
+
+    if t.strftime('%H-%M') in candidate_times:
+        index = candidate_times.index(t.strftime('%H-%M'))
+        print(index)
+        msg = messages.msg[index]['text']
+
+    return msg
+
+
+while True:
+    print('Starting closed loop, checking time...')
+    time.sleep(RECONCILATION_PERIOD_SEC)
+    now = datetime.datetime.now()
+
+    push_text = get_message_body(t=now)
+    if push_text == '':
+        print('>>> Current Time : {} | It is not time to remind. nothing to do.'.format(now))
+        continue
+    else:
+        print('>>> Current Time : {} | Time to remind. Run push-notification.'.format(now))
+        push_data = {
+            'to': LINE_USER_ID,
+            'messages': [
+                {
+                    'type': 'text',
+                    'text': push_text
+                }
+            ]
         }
-    ]
-}
+        pushing_messages(push_data)
 
-DATA = json.dumps(RAW_DATA).encode('utf-8')
-req = urllib.request.Request(
-    url=URL,
-    headers=HEADERS,
-    data=DATA
-)
-
-with urllib.request.urlopen(req) as res:
-    body = res.read()
-
-print(json.loads(body.decode('utf-8')))
