fade03aff1bab32db5679f34c9d1172ff41090d3
hwakabh <hrykwkbys1024@gmail.com>
2019-10-27 10:26:01 +0000

    Added daily-basis reminder to messages.py

--- messages.py
+++ messages.py
@@ -2,14 +2,16 @@
 ## Both English/Japanese message supported with plain texts
 
 msg = [
+    # For dog-walking
     {
         'time': '08-00',
         'text': 'おはよう、08:00 です。\n朝の散歩の時間だよー。'
     },
     {
         'time': '20-00',
-        'text': 'おつかれさま、\n夕方の散歩の時間だよー。\nもうお薬のんでるかな。'
+        'text': 'おつかれさま、\n夕方の散歩の時間だよー。\nお薬のんだかなー。'
     },
+    # For medicine-taking
     {
         'time': '07-45',
         'text': 'おはよう。朝のおくすりの時間だよ。'
@@ -21,5 +23,10 @@ msg = [
     {
         'time': '19-30',
         'text': 'おつかれさま！夜のおくすりの時間だよー。\n散歩行く前に飲んでおくんだよー。'
+    },
+    # For daily-basis
+    {
+        'time': '10-00',
+        'text': 'お散歩おつかれさま！\nお風呂の時間だよー！'
     }
 ]
