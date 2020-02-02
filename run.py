40627462bd68d1fa8aaea99f28036ccdab35630b
hwakabh <hrykwkbys1024@gmail.com>
2020-02-02 20:17:35 +0900

    Updated API kicker modules via line-bot-sdk for pushing messages.

--- run.py
+++ run.py
@@ -3,15 +3,17 @@ from linebot.models import TextSendMessage
 
 import os
 import sys
+import time
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
 
-# Check current time for pushing messaging or not
-def check_times_to_push():
-    pass
 
-
-# Methods to push messages
-def notify_to_user():
-    pass
+# Retrieve contents to push if it's time
+def get_message_context(current_time, conf):
+    for c in conf:
+        if c['time'] == current_time:
+            return c['text']
 
 
 # main functions with closed-loops
@@ -20,17 +22,44 @@ if __name__ == "__main__":
     print('>>> Init: Getting configs for pushing messages')
     USER_ID = os.environ.get('LINE_USER_ID')
     ACCESS_TOKEN = os.environ.get('LINE_ACCESS_TOKEN')
+    TZ = os.environ.get('TZ')
 
     if (USER_ID is None) or (ACCESS_TOKEN is None):
         print('Failed to fetch USER_ID and ACCESS_TOKEN for push messages, confirm to run the commands:')
-        print('\t export LINE_USER_ID=\'USER_ID_TO_PUSH\'\)
-        print('\t export LINE_ACCESS_TOKEN=\'BOT_ACCESS_TOKEN\'\)
+        print('\t export LINE_USER_ID=\'USER_ID_TO_PUSH\'')
+        print('\t export LINE_ACCESS_TOKEN=\'BOT_ACCESS_TOKEN\'')
         sys.exit(1)
+
+    print('>>> Init: Loading contents in messages.py')
+    import messages
+    candidate_times = [c['time'] for c in messages.msg]
+
+    print('>>> Init: Create LINE API Instances')
+    endpoint = LineBotApi(channel_access_token=ACCESS_TOKEN)
 
     print('>>> Starting closed-loops for checking time ...')
     while True:
-        if check_times_to_push:
-            print('Time to notify message for user.')
-            notify_to_user()
+        t = datetime.now().strftime('%H-%M')
+        # Modify timezone if not configured
+        if os.getenv('TZ') != 'Asia/Tokyo':
+            tz = timezone(timedelta(hours=9), 'JST')
+            t = datetime.now(tz).strftime('%H-%M')
+
+        if (t in candidate_times):
+            MESSAGE = get_message_context(
+                current_time=t,
+                conf=messages.msg
+                )
+            print('Calling LINE API with SDK method push_message() ...')
+            endpoint.push_message(
+                USER_ID,
+                TextSendMessage(
+                    text=MESSAGE
+                )
+            )
         else:
+            print('Current time: {} : nothing to do for messaging.'.format(datetime.now()))
             pass
+
+        # Reconcilation Loop interval is 1 min by default
+        time.sleep(60)
